f7b3d0169b88f5c194a579d0bbbe9146ca2e55f0
Ariful Islam <ariful.pust@gmail.com>
2018-09-10 15:49:27 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dream71/bottomnevigetionlibrary/Fragment1.java
@@ -0,0 +1,27 @@
+package com.example.dream71.bottomnevigetionlibrary;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Fragment1 extends Fragment {
+
+    @BindView(R.id.text)
+    TextView textView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_1 , container , false);
+        ButterKnife.bind(this , view);
+        textView.setText("Fragment_1");
+       return view;
+    }
+}
